be65040fcd2fed82fc64fac8e88874f714e31494
Daniil Polovinkin <dolovinkin@gmail.com>
2015-11-01 13:49:35 +0300

    Added NewsItem model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class NewsItem(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
