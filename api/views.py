f139c43271c2da1ec64b38e5417cbaf17a57d31f
Daniil Polovinkin <dolovinkin@gmail.com>
2015-11-01 14:58:13 +0300

    PEP8

--- api/views.py
+++ api/views.py
@@ -6,6 +6,9 @@ from .models import NewsItem
 def get_news(request):
     if request.method == 'GET':
         news = NewsItem.objects.order_by('-pub_date')[:10]
-        return HttpResponse(serializers.serialize('json', news), content_type='application/json')
+        return HttpResponse(
+            serializers.serialize('json', news),
+            content_type='application/json'
+        )
     else:
         return HttpResponse(status=405)
