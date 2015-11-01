2245b75792f6d980bfa41887e566930ccdc6b6df
Daniil Polovinkin <dolovinkin@gmail.com>
2015-11-01 18:54:46 +0300

    Added post news view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.core import serializers
+from django.utils import timezone
+from django.utils.html import strip_tags, escape
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -16,6 +18,21 @@ def get_news(request):
         )
     else:
         return HttpResponse(status=405)
+
+
+def post_news(request):
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            new = NewsItem()
+            new.title = escape(strip_tags(json_data['title']))
+            new.body = escape(strip_tags(json_data['body']))
+            new.pub_date = timezone.now()
+            new.save()
+        else:
+            return HttpResponse(status=405)
+    else:
+        return HttpResponse(status=403)
 
 
 @csrf_exempt
