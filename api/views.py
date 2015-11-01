2977956532510b3d5eabdf5c95ef1a719ab4bcb4
Daniil Polovinkin <dolovinkin@gmail.com>
2015-11-01 14:43:36 +0300

    Get news now outputs last 10 rows from NewsItem in json

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.core import serializers
+from .models import NewsItem
 
 
 def get_news(request):
     if request.method == 'GET':
-        return HttpResponse('It works')
+        news = NewsItem.objects.order_by('-pub_date')[:10]
+        return HttpResponse(serializers.serialize('json', news))
     else:
         return HttpResponse(status=405)
